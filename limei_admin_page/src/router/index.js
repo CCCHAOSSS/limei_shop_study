f62d31f482435808d6e7a4642ca8b011b0eb5beb
limei <lmliang11@foxmail.com>
2024-03-14 19:22:27 +0800

    feat:Add Vue style

--- limei_admin_page/src/router/index.js
+++ limei_admin_page/src/router/index.js
@@ -1,23 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Login from '../views/Login.vue'
+// 配置路由规则
+const constRouter = [
+  // 重定向
+  {
+    path: '',
+    redirect: "/login"
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+]
 
+// 创建路由
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  // 使用路路由规则常量
+  routes: constRouter
 })
 
+// 导出router
 export default router
