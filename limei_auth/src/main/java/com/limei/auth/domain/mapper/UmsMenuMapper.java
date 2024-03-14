88828475f2141e64efa86f776c11d85c1ccf9ac6
limei <lmliang11@foxmail.com>
2024-03-14 19:22:52 +0800

    refactor

--- limei_auth/src/main/java/com/limei/auth/domain/mapper/UmsMenuMapper.java
+++
@@ -1,9 +0,0 @@
-package com.limei.auth.domain.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.limei.auth.domain.entry.UmsMenu;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UmsMenuMapper extends BaseMapper<UmsMenu> {
-}
